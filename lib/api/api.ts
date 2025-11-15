5fbd3cd3f85ad25b76d0ae3a2e0c58b6b2cfdc84
Maksim B <makcnma2@ua.fm>
2025-11-15 13:13:46 +0200

    fix

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-
-const myKey: string = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN!;
-
 export const nextServer = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL + '/api',
   withCredentials: true,
-  headers: {
-    Authorization: `Bearer ${myKey}`,
-  }
 });
 
